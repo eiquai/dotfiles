63ecd9c148d2444429b6bf811c79738f6b896d88
eiquai <eiquai@posteo.de>
2017-11-11 18:03:42 +0100

    reset fontsize for new QHD-Screen

---
+++ bin/compton-reinit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+compton -f -I 0.10 -O 0.10 --backend glx --vsync opengl &
--- xmonad.hs
+++ xmonad.hs
@@ -1,13 +1,16 @@
 import XMonad
+import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageHelpers
+--import XMonad.Hooks.EwmhDesktops as EwmhDesktop --ambigous with fullscreenEventHook
 import XMonad.Util.Paste
 import XMonad.Util.NamedWindows
 import XMonad.Util.Run(spawnPipe, safeSpawn)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Scratchpad
 import XMonad.Layout.Grid
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.ThreeColumns
@@ -16,7 +19,6 @@ import XMonad.Layout.Fullscreen
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Layout.ToggleLayouts
 import XMonad.StackSet as W
---import XMonad.Hooks.EwmhDesktops --ambigous with fullscreenEventHook
 import Graphics.X11.ExtraTypes.XF86
 import System.IO
 import Data.Ratio ((%))
@@ -36,10 +38,9 @@ import Data.Ratio ((%))
 
 -- VARIABLES FOR XMOBAR
 myTitleColor        =   "#272822"
-myTitleLength       =   40
+myTitleLength       =   60
 myCurrentWSColor    =   "#F92672"
 myVisibleWSColor    =   "#66D9EF"
---myUrgentWSColor     =   "#F92672"
 myUrgentWSColor     =   "#F92672"
 myUrgentWSLeft      =   "!"
 myUrgentWSRight     =   "!"
@@ -56,7 +57,7 @@ instance UrgencyHook LibNotifyUrgencyHook where
         Just idx    <- fmap (W.findTag w) $ gets windowset
         safeSpawn "notify-send" [show name, "workspace" ++ idx]
 
-myManageHook = floatHook <+> fullscreenManageHook
+myManageHook = floatHook <+> fullscreenManageHook <+> manageScratchPad
 
 floatHook = composeAll
     [ className =? "gimp"   --> doFloat
@@ -68,27 +69,43 @@ floatHook = composeAll
     , resource =? "gnome-control-center" --> doFloat
     , resource =? "gnome-weather" --> doFloat]
 
+manageScratchPad::ManageHook
+manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
+
+    where
+
+    h = 0.6
+    w = 0.6
+    l = 0.2
+    t = 0.4
+
 myStartupHook ::X ()
 myStartupHook = do
-    spawn "compton -f -I 0.10 -O 0.10 --backend glx --vsync opengl"
+     spawn "compton -f -I 0.10 -O 0.10 --backend glx --vsync opengl"
+ 
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+--withUrgencyHook LibNotifyUrgencyHook <- This still is ToDo!
+
+myPP = xmobarPP
+            { ppCurrent = xmobarColor "#F92672" "" . wrap "[" "]"
+            , ppTitle = xmobarColor "#F92672" "#272822" . shorten myTitleLength}
+
+--myPP = xmobarPP
+--             { ppTitle = xmobarColor "#F92672" "#272822" . shorten myTitleLength
+--             , ppCurrent =   xmobarColor myCurrentWSColor "" . wrap myCurrentWSLeft myCurrentWSRight
+--             , ppVisible =   xmobarColor myVisibleWSColor "" . wrap myVisibleWSLeft myVisibleWSRight 
+--             , ppUrgent  =   xmobarColor myUrgentWSColor  "" . wrap myUrgentWSLeft myUrgentWSRight 
+--             }
+
+myBar = "xmobar ~/dotfiles/xmobarrc"
 
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_m)
 
-main = do
-    xmproc <- spawnPipe "xmobar"
-
-    xmonad $ withUrgencyHook LibNotifyUrgencyHook $ def { 
+myConfig = def {
         manageHook      = ( isFullscreen --> doFullFloat ) <+> manageDocks <+> myManageHook <+> manageHook def
-        , layoutHook    = avoidStruts $ toggleLayouts (noBorders Full) $ smartBorders $ layoutHook def
         , startupHook   = myStartupHook <+> startupHook def
-        , handleEventHook   =   handleEventHook def <+> fullscreenEventHook <+> docksEventHook
-        , logHook = dynamicLogWithPP $ def 
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#F92672" "#272822" . shorten myTitleLength
-                        , ppCurrent =   xmobarColor myCurrentWSColor "" . wrap myCurrentWSLeft myCurrentWSRight
-                        , ppVisible =   xmobarColor myVisibleWSColor "" . wrap myVisibleWSLeft myVisibleWSRight 
-                        , ppUrgent  =   xmobarColor myUrgentWSColor  "" . wrap myUrgentWSLeft myUrgentWSRight 
-                        }
+        , handleEventHook   =   handleEventHook def <+> fullscreenEventHook
+        , layoutHook    = avoidStruts $ toggleLayouts (noBorders Full) $ smartBorders $ layoutHook def
         , modMask                 = myModMask
         , terminal                = myTerminal
         , XMonad.workspaces       = myWorkSpaces
@@ -104,7 +121,8 @@ main = do
         , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +1%")
         , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -1%")
         , ((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
-        , ((controlMask .|. mod1Mask, xK_t), spawn "urxvt")
+        , ((controlMask .|. mod1Mask, xK_t), spawn myTerminal)
+        , ((mod1Mask .|. shiftMask, xK_comma), scratchpad) --urxvt quake-style
         , ((controlMask, xK_space), spawn "synapse")
         , ((0, xF86XK_Tools), spawn "systemctl suspend")
         , ((0, 0x1008FF21), spawn "systemctl suspend")
@@ -116,9 +134,13 @@ main = do
         , ((0, xK_Insert), pasteSelection) -- there is a problem here, as it seems to escape some characters
         ]
 
+    where
+
+    scratchpad = scratchpadSpawnActionTerminal myTerminal
+
 myTerminal              = "urxvt"
 myModMask               = mod4Mask -- [super]
 myBorderWidth           = 1
 myNormalBorderColor     = "#e0e0e0"
 myFocusedBorderColor    = "#F92672"
-myWorkSpaces    = [ "1:www", "2:bash", "3:music", "4:mail", "5:firefox", "6:calc", "7:misc", "8:misc", "9:misc"]
+myWorkSpaces    = [ "1:<fn=1>\xf268</fn>", "2:<fn=1>\xf120</fn>", "3:<fn=1>\xf001</fn>", "4:<fn=1>\xf2b6</fn>", "5:<fn=1>\xf269</fn>", "6:calc", "7:misc", "8:misc", "9:misc"]
